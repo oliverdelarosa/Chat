93316f18615c92798773db415bf809d645fe5682
oliver de la rosa <oliverdelarosavictorio>
2022-08-01 18:28:02 -0500

    arreglarlo que no funciona

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-const socket = io("http://localhost:4000/");
+//const socket = io("http://localhost:4000/");
+const socket = io("/");
 
 function App() {
   const [message, setMessage] = useState("");
